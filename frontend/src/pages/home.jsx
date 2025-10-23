69050d891f103ce63d8ab7207c235ae6a27f26a0
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-10-23 16:58:50 +0530

    day12

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -4,7 +4,7 @@ import Image from "../assets/shoes.webp";
 const Home = () => {
   return (
     <>
-{/*       
+      
       <div className="bg-gray-100 min-h-screen flex flex-col md:flex-row items-center justify-center px-10 py-20">
         <div className="md:w-1/2 text-center md:text-left space-y-6">
           <h1 className="text-6xl font-extrabold text-gray-900 leading-tight">
@@ -26,7 +26,7 @@ const Home = () => {
             className="w-full h-auto max-h-[80vh] object-cover"
           />
         </div>
-      </div> */}
+      </div>
     </>
   );
 };
