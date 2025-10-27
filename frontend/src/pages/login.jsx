22b7674d58c5e71e5bf54c730a387bc5ede4c05e
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-10-27 16:52:55 +0530

    day18

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -4,7 +4,7 @@ import BgImage from "../assets/login.jpg"; // Ensure the image is in the correct
 // Login Component
 const Login = () => {
   return (
-    
+    // Main Container
     <div
       className="min-h-screen flex items-center justify-center bg-cover bg-center relative"
       style={{ backgroundImage: `url(${BgImage})` }} // Background image applied
