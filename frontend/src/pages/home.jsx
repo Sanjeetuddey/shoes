2f387e5d66ce95ca7b7cc6854ddd3c76b0764b07
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-10-28 23:16:33 +0530

    day21

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -5,9 +5,7 @@ import Image from "../assets/shoes.webp";
 const Home = () => {
   return (
     <>
-   //main pae 
-         //home page content
-      <div className="bg-gray-100 min-h-screen flex flex-col md:flex-row items-center justify-center px-10 py-20">
+         <div className="bg-gray-100 min-h-screen flex flex-col md:flex-row items-center justify-center px-10 py-20">
         <div className="md:w-1/2 text-center md:text-left space-y-6">
           <h1 className="text-6xl font-extrabold text-gray-900 leading-tight">
             Step Into <span className="text-amber-500">Comfort & Style</span>
